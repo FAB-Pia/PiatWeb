c1a90939109a6ac2ab780b16f363bf2d671bb273
FelixAubets <felixcrat@gmail.com>
2022-03-02 08:55:00 +0100

    Things

--- pages/index.js
+++ pages/index.js
@@ -53,13 +53,12 @@ export default function Home() {
             </main>
 
             <footer className={styles.footer}>
-                    Gracies a{' '}
+                Gracies a{':'} <br/>
                 <a href='https://dinahosting.com'>
                     <span className={styles.logo}>
                         <Image src="/images/dinahosting.png" alt="Logo DinaHosting" width={72} height={16} />
                     </span>
                 </a>
-                    {''} i{' '}
                 <a href='https://wapps.cat'>
                     <span className={styles.logo}>
                         <Image src="/images/wapps.png" alt="Logo Wapps" width={72} height={16} />
