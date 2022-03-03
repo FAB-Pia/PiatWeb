4b3885ef9c76bafa5dc85fbcd876d60fe8bcdd46
FelixAubets <felixcrat@gmail.com>
2022-03-03 13:14:26 +0100

    Things

--- pages/components/header.js
+++ pages/components/header.js
@@ -2,7 +2,7 @@ import styles from "../../styles/Home.module.css";
 
 export default function Header() {
     return(
-        <header className={styles.footer}>
+        <header className={styles.header}>
             <strong><a href='http://140.238.75.242:3001'>&#8962;</a></strong>
         </header>
     )
