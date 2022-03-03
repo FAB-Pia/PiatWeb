4f6cd50b11d7ee1d8610a67c12e8476549f6681f
FelixAubets <felixcrat@gmail.com>
2022-03-03 13:06:18 +0100

    Things

--- pages/components/header.js
+++ pages/components/header.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Header() {
     return(
         <header className={styles.footer}>
-            <a href='http://140.238.73.242:3001'>&#8962;</a>
+            <a href='http://140.238.75.242:3001'>&#8962;</a>
         </header>
     )
 }
