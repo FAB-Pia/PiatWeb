7f0b8ba708dbd211ebce12dd067845652a447fcc
FelixAubets <felixcrat@gmail.com>
2022-03-03 10:37:31 +0100

    Things

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Footer() {
     return(
         <footer className={styles.footer}>
-            <Image src='/images/Reconeixement-NoComercial-Compartigual.png' width={144} height={32} />
+            <Image src='/images/Reconeixement-NoComercial-CompartirIgual.png' width={144} height={32} />
             Gracies a{':'} <br/>
             <div>
                 <a href='https://dinahosting.com' target='_blank' rel="noreferrer">
