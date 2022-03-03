e51a7322bdd1b01fb6571f8bd8aa7308923ae92e
FelixAubets <felixcrat@gmail.com>
2022-03-03 10:10:30 +0100

    Things

--- pages/google.js
+++ pages/google.js
@@ -22,11 +22,11 @@ export default function Google() {
                 </h1>
 
                 <div className={styles.grid}>
-                    <div onClick='openInNewTab("example.com")' className={styles.card}>
+                    <div onClick={openInNewTab("example.com")} className={styles.card}>
                         <img src="/logos_serveis/sheets.png" alt='Logo Sheets' height='100' width='100' />
                         <h2>Sheets &rarr;</h2>
                     </div>
-                    <div onClick='openInNewTab("example.com")' className={styles.card}>
+                    <div onClick={openInNewTab("example.com")} className={styles.card}>
                         <img src="/logos_serveis/docs.png" alt='Logo Docs' height='70' width='70' />
                         <h2>Docs &rarr;</h2>
                     </div>
