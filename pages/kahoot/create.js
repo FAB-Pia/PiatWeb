c59e20889faf4a47aecf46cada7e578415cb43d5
FelixAubets <felixcrat@gmail.com>
2022-03-03 12:29:36 +0100

    Things

--- pages/kahoot/create.js
+++ pages/kahoot/create.js
@@ -110,6 +110,7 @@ export default function KahootCreate() {
                         }
                     }
                 })}
+                {guideHTML}
             </main>
 
             <Footer />
