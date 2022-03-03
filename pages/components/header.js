f0feda4271a7ead8b7534dba39dde6e8656573fe
FelixAubets <felixcrat@gmail.com>
2022-03-03 13:17:31 +0100

    Things

--- pages/components/header.js
+++ pages/components/header.js
@@ -3,7 +3,7 @@ import styles from "../../styles/Home.module.css";
 export default function Header() {
     return(
         <header className={styles.header}>
-            <strong>&#8592;&#8962;</strong>
+            <strong>&larr;&#8962;</strong>
         </header>
     )
 }
