d48253b5c89894a71bbd94bb2e1195db24f0a4e6
FelixAubets <felixcrat@gmail.com>
2022-03-03 09:39:49 +0100

    Things

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,10 @@ export default function Home() {
 
                 <div className={styles.grid}>
                     <a href="https://nextjs.org/docs" className={styles.card}>
+                        <h2>Google &rarr;</h2>
+                        <p>Find in-depth information about Next.js features and API.</p>
+                    </a>
+                    <a href="https://nextjs.org/docs" className={styles.card}>
                         <h2>Documentation &rarr;</h2>
                         <p>Find in-depth information about Next.js features and API.</p>
                     </a>
