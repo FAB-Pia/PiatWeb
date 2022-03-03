92d68afbcee5002e2024a30ec14229034bac4d62
FelixAubets <felixcrat@gmail.com>
2022-03-03 09:46:10 +0100

    Things

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
 
                 <div className={styles.grid}>
                     <a href="https://nextjs.org/docs" className={styles.card}>
-                        <img src="/logos_serveis/google.png" alt='Logo Google' height='10' width='10' />
+                        <img src="/logos_serveis/google.png" alt='Logo Google' height='100' width='100' />
                         <h2>Google &rarr;</h2>
                     </a>
                     <a href="https://nextjs.org/docs" className={styles.card}>
