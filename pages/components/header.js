805f57d9974af4b26f52625993593d1e151dd751
FelixAubets <felixcrat@gmail.com>
2022-03-04 09:10:00 +0100

    Things

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,11 +1,10 @@
 import styles from "../../styles/Home.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 export default function Header() {
     return(
         <header className={styles.header}>
-            <strong><FontAwesomeIcon icon={solid('arrow-left')} /><FontAwesomeIcon icon={solid('house')} /></strong>
+            <strong><FontAwesomeIcon icon="fa-solid fa-arrow-left" /><FontAwesomeIcon icon="fa-solid fa-house-blank" /></strong>
         </header>
     )
 }
