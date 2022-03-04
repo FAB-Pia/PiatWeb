f1a1c7f6cc9164cb06bf078815df70ccb1d5ca03
FelixAubets <felixcrat@gmail.com>
2022-03-04 09:53:53 +0100

    Things

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,7 +1,7 @@
 import styles from "../../styles/Home.module.css";
 import fastyles from '@fortawesome/fontawesome-free/css/all.css'
 
-const website_url = 'https://140.238.75.242:3001'
+const website_url = 'http://140.238.75.242:3001'
 
 export default function Header() {
     return(
