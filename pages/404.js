6f6754f9585905339721c48e7e15b701a505695d
FelixAubets <felixcrat@gmail.com>
2022-03-16 09:03:36 +0100

    Things

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Header from "./components/header";
+
+export default function Custom404() {
+    return (
+        <div>
+            <Header />
+            <h1>404 - Page Not Found</h1>
+        </div>
+    );
+}
