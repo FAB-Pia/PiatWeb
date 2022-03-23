eb6bf2a887ace7cfd0ff0561521134cda5d830bd
2eso <mjh>
2022-03-23 09:58:26 +0100

    Things

--- pages/google/docs/crear.js
+++ pages/google/docs/crear.js
@@ -24,7 +24,7 @@ const guide = {
             },
             {
                 "type":"image",
-                "location":"/images/google/docs/docs2.png"
+                "location":"/images/google/docs/docs2.jpg"
             },
             {
                 "type":"text",
