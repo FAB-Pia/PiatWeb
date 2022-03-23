0a9488e0f7c7d626eb7d7f64341200873b28628e
2eso <mjh>
2022-03-23 10:03:24 +0100

    Things

--- pages/google/sheets/grafics.js
+++ pages/google/sheets/grafics.js
@@ -1,8 +1,8 @@
-import styles from "../../styles/Kahoot.module.css";
+import styles from "../../../styles/Kahoot.module.css";
 import Head from "next/head";
-import Footer from "../components/footer";
+import Footer from "../../components/footer";
 import {useEffect} from "react";
-import Header from "../components/header";
+import Header from "../../components/header";
 
 const guide = {
     "title":"Gràfics",
@@ -26,7 +26,7 @@ const guide = {
 };
 let guideHTML = "";
 
-export default function KahootCreate() {
+export default function GoogleSheetsGraph() {
     useEffect(() => {
         guide.body.forEach(item => {
             if (item.type === 'image') {
